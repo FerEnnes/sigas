127f925378015633c7a162b7ccaeabfd410b4982
MKT AMO <mktamo@iMac-de-MKT.local>
2025-04-10 18:07:35 -0300

    tela de configurações

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// src/App.js
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify'; 
-import 'react-toastify/dist/ReactToastify.css';  
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 import SupplierListPage from './pages/SupplierListPage';
 import SupplierFormPage from './pages/SupplierFormPage';
@@ -18,57 +18,37 @@ import UsuarioFormPage from './pages/UsuarioFormPage';
 import LoginPage from './pages/LoginPage';
 import RecuperarSenhaPage from './pages/RecuperarSenhaPage';
 import RedefinirSenhaPage from './pages/RedefinirSenhaPage';
+import ConfiguracoesPage from './pages/ConfiguracoesPage'; 
 import NotFoundPage from './pages/NotFoundPage';
-
-import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Login e recuperação */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/recuperar-senha" element={<RecuperarSenhaPage />} />
         <Route path="/redefinir-senha/:token" element={<RedefinirSenhaPage />} />
+        <Route path="/configuracoes" element={<ConfiguracoesPage />} />
 
-        {/* Redirecionamento padrão */}
         <Route path="/" element={<Navigate to="/fornecedores" />} />
-
-        {/* Usuários */}
         <Route path="/usuarios" element={<UsuarioListPage />} />
         <Route path="/usuarios/cadastrar" element={<UsuarioFormPage />} />
-
-        {/* Clientes */}
         <Route path="/clientes" element={<ClienteListPage />} />
         <Route path="/clientes/cadastrar" element={<ClienteFormPage />} />
-
-        {/* Fornecedores */}
         <Route path="/fornecedores" element={<SupplierListPage />} />
         <Route path="/fornecedores/cadastrar" element={<SupplierFormPage />} />
-
-        {/* Propriedades */}
         <Route path="/propriedades" element={<PropertyListPage />} />
         <Route path="/propriedades/cadastrar" element={<PropertyFormPage />} />
-
-        {/* Plano de Contas */}
         <Route path="/plano-contas" element={<PlanoContasPage />} />
-
-        {/* Contas */}
         <Route path="/contas/pagar" element={<ContasPagarPage />} />
         <Route path="/contas/receber" element={<ContasReceberPage />} />
-
-        {/* Calendário */}
         <Route path="/calendario" element={<CalendarioPage />} />
-
-        {/* Página 404 */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
 
-      {/*  Toast container global */}
       <ToastContainer position="top-right" autoClose={3000} />
     </Router>
   );
 }
 
 export default App;
-
